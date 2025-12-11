5b2188d4e1b555ad57e066e4085ba5f0f547b048
emmemeno <pompelmo@gmail.com>
2025-12-11 12:13:17 +0100

    day 11 wip (push before jumping to laptop)

--- src/day_eleven.rs
+++ src/day_eleven.rs
@@ -1,1 +1,178 @@
 #![allow(dead_code)]
+
+use std::{cell::RefCell, fmt::Display, rc::Rc};
+
+use crate::InputMode;
+
+type NodeId = [char; 3];
+
+struct Node {
+    id: NodeId,
+    output_links: Vec<Rc<RefCell<Node>>>
+}
+
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let format_id = |n: NodeId| format!("{}{}{}", n[0], n[1], n[2]);
+        let mut output = format!("{} ->", format_id(self.id));
+        for link in self.output_links.iter() {
+            output = format!("{} {}", output, format_id((*link).borrow().id));
+        }
+        write!(f, "{}\n", output)
+    }
+}
+
+impl Node {
+    fn from_str(input: &str) -> Self {
+        let (part_one, _) = input.split_once(":").unwrap(); 
+        let id: NodeId = part_one.chars().collect::<Vec<_>>().try_into().unwrap();
+        Self {id, output_links: vec![] }
+    }
+    fn add_connection(&mut self, new_link: Rc<RefCell<Node>>) {
+        self.output_links.push(new_link);
+    }
+}
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id   // compare only this field
+    }
+}
+
+impl Eq for Node {}
+struct Graph {
+    nodes: Vec<Rc<RefCell<Node>>>
+}
+
+impl Graph {
+    fn new() -> Self {
+        Self { nodes: vec![] }
+
+    }
+
+    fn get_node(&self, key: NodeId) -> Option<Rc<RefCell<Node>>> {
+        return self.nodes.iter().find_map(|n| {
+            if n.borrow().id == key {
+                Some(n.clone())
+            }  else {
+                None
+            }
+        });
+    }
+
+    fn add_node(&mut self, node: Node) -> bool{
+        if self.nodes.iter().any(|n| n.borrow().id == node.id) {
+            return false;
+        } else {
+            self.nodes.push(Rc::new(RefCell::new(node)));
+            return true;
+        }
+    }
+
+    fn add_connections_from_str(&mut self, input_line: &str) -> Result<String, String> {
+        let (part_one, part_two) = input_line.split_once(":").unwrap(); 
+        let parent_id: NodeId = part_one.chars().collect::<Vec<_>>().try_into().unwrap();
+        let parent_node = if let Some(n) = self.get_node(parent_id) {
+            n
+        } else {
+            return Result::Err("Parent node {part_one} not found".to_string());
+        };
+
+        for link in part_two.trim().split(" ") {
+            let child_id: NodeId = link.chars().collect::<Vec<_>>().try_into().unwrap();
+            let child_node = if let Some(n) = self.get_node(child_id) {
+                n
+            } else {
+                return Err("Child node {link} not found".to_string());
+            };
+            parent_node.borrow_mut().add_connection(child_node.clone());
+        }
+        return Ok("Connections created".to_string());
+    }
+
+    fn pathfinding(&self, from: NodeId, to: NodeId, mut current_path: Vec<Rc<RefCell<Node>>>) -> Vec<Rc<RefCell<Node>>>{
+        // if no path, add the starting point
+        if current_path.len() == 0 {
+            current_path.push(self.get_node(from).unwrap());
+        }
+        // set frontier at last visited path node
+        let frontier = current_path.last().unwrap().clone();
+        let max_distance = 100;
+            if frontier.borrow().id == to {
+                println!("Target reached");
+                return current_path;
+            }
+            for child in frontier.borrow().output_links.iter() {
+                // println!("From {} to {}", frontier.borrow(), child.borrow());
+                // dont visit the same node two times 
+                if !current_path.contains(child) {
+                    println!("bau");
+                    current_path.push(child.clone());
+                    current_path = self.pathfinding(from, to, current_path.clone());
+                } else {
+                    break;
+                }
+            }
+        unreachable!();
+    }
+
+}
+
+impl Display for Graph {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut output = String::new();
+        for node in self.nodes.iter() {
+           output = format!("{}{}", output, node.borrow()); 
+        }
+        write!(f, "{}", output)
+    }
+}
+
+fn generate_graph(input: String) -> Graph {
+    // first create all nodes:
+    let out_node = Node { id: ['o','u','t'], output_links: vec![] };
+    let mut graph = Graph::new();
+    graph.add_node(out_node);
+    for line in input.lines() {
+        graph.add_node(Node::from_str(line));
+    }
+    // create connections
+    for line in input.lines() {
+        _ = graph.add_connections_from_str(line);
+    }
+    graph
+}
+
+fn parse(mode: InputMode) -> Graph {
+    let input: String;
+    match mode {
+        InputMode::Example => {
+            input =
+                "aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+".to_string();
+        }
+        InputMode::Normal => {
+            input = super::load_input("input/input-day11");
+        }
+    }
+    generate_graph(input)
+}
+
+pub fn part_one() {
+    let graph = parse(InputMode::Example);
+    println!("{graph}");
+    let path = graph.pathfinding(['y','o','u'], ['o','u','t'], vec![]);
+    println!("What now?");
+    // for n in path {
+    //     println!("{}", n.borrow());
+    // }
+
+}
--- src/day_ten.rs
+++ src/day_ten.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 
 use super::InputMode;
 
@@ -13,7 +13,7 @@ struct Machine {
     target_joltage: Vec<u16>,
     buttons: Vec<Button>,
     // how many trigger for joltage are repeated in buttons
-    joltage_triggers: HashMap<Trigger, u8>
+    joltage_triggers: HashMap<Trigger, u8>,
 }
 
 impl Machine {
@@ -30,17 +30,17 @@ impl Machine {
             match light {
                 '#' => target_lights.push(true),
                 '.' => target_lights.push(false),
-                _ => unreachable!()
+                _ => unreachable!(),
             }
         }
         let mut buttons: Vec<Button> = vec![];
         let mut joltage_triggers: HashMap<Trigger, u8> = HashMap::new();
         for button_str in buttons_str.trim().split(" ") {
             let mut button: Button = vec![];
-            let button_triggers = &button_str[1..button_str.len()-1];
+            let button_triggers = &button_str[1..button_str.len() - 1];
             for trigger in button_triggers.split(",") {
                 let trigger = trigger.parse::<Trigger>().unwrap();
-                button.push(trigger); 
+                button.push(trigger);
                 if let Some(t) = joltage_triggers.get_mut(&trigger) {
                     *t = *t + 1;
                 } else {
@@ -50,14 +50,19 @@ impl Machine {
             buttons.push(button);
         }
         let mut target_joltage: Vec<u16> = vec![];
-        let joltage_str = &joltage_str[..joltage_str.len()-1];
+        let joltage_str = &joltage_str[..joltage_str.len() - 1];
         for j in joltage_str.split(",") {
             target_joltage.push(j.parse::<u16>().unwrap());
         }
         // println!("Target Lights: {target_lights:?}");
         // println!("Buttons: {buttons:?}");
         // println!("Joltage Requirement: {joltage_req:?}");
-        Self { target_lights, target_joltage, buttons, joltage_triggers}
+        Self {
+            target_lights,
+            target_joltage,
+            buttons,
+            joltage_triggers,
+        }
     }
 
     // ##############################
@@ -66,23 +71,31 @@ impl Machine {
 
     fn most_beneficial_buttons_for_light(&self, target: &[bool]) -> Vec<(ButtonId, Score)> {
         // println!("Most beneficial buttons for target: {target:?}");
-        let light_to_switch: HashSet<u8> = target.iter().enumerate().filter(|(_, l)| **l).map(|(n, _)| n as u8).collect();
+        let light_to_switch: HashSet<u8> = target
+            .iter()
+            .enumerate()
+            .filter(|(_, l)| **l)
+            .map(|(n, _)| n as u8)
+            .collect();
         let mut button_ladder: Vec<(ButtonId, Score)> = vec![];
         for (n, button) in self.buttons.iter().enumerate() {
             // if button trigger off the light, its positive effect (add to score)
-            let score = button.iter().filter(|&trigger| light_to_switch.contains(trigger)).count() as i32;
+            let score = button
+                .iter()
+                .filter(|&trigger| light_to_switch.contains(trigger))
+                .count() as i32;
             button_ladder.push((n, score));
         }
         button_ladder.sort_by_key(|&(_, count)| -(count as i16));
         // give priority to larger buttons (on equal arm)
-        button_ladder.sort_by(|(a_id, a_score), (b_id, b_score)|
-            match b_score.cmp(a_score) {
+        button_ladder.sort_by(
+            |(a_id, a_score), (b_id, b_score)| match b_score.cmp(a_score) {
                 std::cmp::Ordering::Less => std::cmp::Ordering::Less,
                 std::cmp::Ordering::Equal => {
-                    self.buttons[*b_id].len().cmp(&self.buttons[*a_id].len())  
-                },
+                    self.buttons[*b_id].len().cmp(&self.buttons[*a_id].len())
+                }
                 std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
-            }
+            },
         );
         button_ladder
     }
@@ -92,13 +105,19 @@ impl Machine {
         let mut new_target = target.to_vec();
         for trigger in &self.buttons[button] {
             let current_status = target[*trigger as usize];
-            new_target[*trigger as usize]= !current_status;
+            new_target[*trigger as usize] = !current_status;
         }
         new_target
     }
 
-    fn button_light_combo_only_length(&self, actual_combo_length: usize, actual_target: &[bool], ladder: &mut Vec<Vec<Button>>, shortest_sequence: &mut usize) -> usize {
-        let mbb = self.most_beneficial_buttons_for_light(&actual_target); 
+    fn button_light_combo_only_length(
+        &self,
+        actual_combo_length: usize,
+        actual_target: &[bool],
+        ladder: &mut Vec<Vec<Button>>,
+        shortest_sequence: &mut usize,
+    ) -> usize {
+        let mbb = self.most_beneficial_buttons_for_light(&actual_target);
         for (_n, (button_id, _)) in mbb.into_iter().enumerate() {
             let new_target = self.apply_button_to_lights(button_id, &actual_target);
             // println!("New target: {new_target:?}");
@@ -110,14 +129,25 @@ impl Machine {
                 // println!("Combination: {new_combo:?}");
                 break;
             } else if actual_combo_length + 2 < *shortest_sequence && *shortest_sequence != 1 {
-                self.button_light_combo_only_length(actual_combo_length + 1, &new_target, ladder, shortest_sequence);
+                self.button_light_combo_only_length(
+                    actual_combo_length + 1,
+                    &new_target,
+                    ladder,
+                    shortest_sequence,
+                );
             }
         }
         *shortest_sequence
     }
 
-    fn button_light_combo_verbose(&self, actual_combo: &Vec<Button>, actual_target: &[bool], ladder: &mut Vec<Vec<Button>>, shortest_sequence: &mut usize) -> Vec<Vec<Button>> {
-        let mbb = self.most_beneficial_buttons_for_light(&actual_target); 
+    fn button_light_combo_verbose(
+        &self,
+        actual_combo: &Vec<Button>,
+        actual_target: &[bool],
+        ladder: &mut Vec<Vec<Button>>,
+        shortest_sequence: &mut usize,
+    ) -> Vec<Vec<Button>> {
+        let mbb = self.most_beneficial_buttons_for_light(&actual_target);
         for (_n, (button_id, _)) in mbb.into_iter().enumerate() {
             let mut new_combo = actual_combo.clone();
             new_combo.push(self.buttons[button_id].clone());
@@ -128,10 +158,14 @@ impl Machine {
                     *shortest_sequence = new_combo.len();
                 }
                 ladder.push(new_combo.clone());
-                println!("Found a new top score ({}) combination for {:?}", new_combo.len(), self.target_lights);
+                println!(
+                    "Found a new top score ({}) combination for {:?}",
+                    new_combo.len(),
+                    self.target_lights
+                );
                 // println!("Combination: {new_combo:?}");
                 break;
-            } else if new_combo.len() + 1< *shortest_sequence {
+            } else if new_combo.len() + 1 < *shortest_sequence {
                 self.button_light_combo_verbose(&new_combo, &new_target, ladder, shortest_sequence);
             }
         }
@@ -143,15 +177,12 @@ impl Machine {
     // ##############################
 
     fn most_beneficial_buttons_for_joltage(&self, target: &[u16]) -> Vec<(ButtonId, Score)> {
-        
         let min_in_target = target.iter().enumerate().min().unwrap();
         let max_in_target = target.iter().enumerate().max().unwrap();
         let mut button_ladder: Vec<(ButtonId, Score)> = vec![];
         // if target joltage is zero, exclude the button
         for (n, button) in self.buttons.iter().enumerate() {
-            if button.iter().any(|pos| {
-                target[*pos as usize] == 0
-                }) {
+            if button.iter().any(|pos| target[*pos as usize] == 0) {
                 // println!("Filtering {button:?} for {target:?}");
                 continue;
             }
@@ -182,8 +213,14 @@ impl Machine {
         new_target
     }
 
-    fn button_joltage_combo_only_length(&self, actual_combo_length: usize, actual_target: &[u16], shortest_sequence: &mut usize, max_deep_search: usize) -> usize {
-        let mbb = self.most_beneficial_buttons_for_joltage(&actual_target); 
+    fn button_joltage_combo_only_length(
+        &self,
+        actual_combo_length: usize,
+        actual_target: &[u16],
+        shortest_sequence: &mut usize,
+        max_deep_search: usize,
+    ) -> usize {
+        let mbb = self.most_beneficial_buttons_for_joltage(&actual_target);
         for (_n, (button_id, _)) in mbb.into_iter().enumerate() {
             let new_target = self.apply_button_to_joltage(button_id, &actual_target);
             // println!("New target: {new_target:?}");
@@ -191,17 +228,25 @@ impl Machine {
                 if actual_combo_length + 1 < *shortest_sequence {
                     *shortest_sequence = actual_combo_length + 1;
                 }
-                println!("Found a new top score ({}) combination for {:?}", actual_combo_length, self.target_joltage);
+                println!(
+                    "Found a new top score ({}) combination for {:?}",
+                    actual_combo_length, self.target_joltage
+                );
                 break;
             } else if actual_combo_length + 2 < *shortest_sequence {
-                self.button_joltage_combo_only_length(actual_combo_length + 1, &new_target, shortest_sequence, max_deep_search);
+                self.button_joltage_combo_only_length(
+                    actual_combo_length + 1,
+                    &new_target,
+                    shortest_sequence,
+                    max_deep_search,
+                );
             }
         }
         *shortest_sequence
     }
 
     // fn button_joltage_combo_verbose(&self, actual_combo: &Vec<Button>, actual_target: &[u16], ladder: &mut Vec<Vec<Button>>, shortest_sequence: &mut usize) -> Vec<Vec<Button>> {
-    //     let mbb = self.most_beneficial_buttons_for_joltage(&actual_target); 
+    //     let mbb = self.most_beneficial_buttons_for_joltage(&actual_target);
     //     for (_n, (button_id, _)) in mbb.into_iter().enumerate() {
     //         let mut new_combo = actual_combo.clone();
     //         new_combo.push(self.buttons[button_id].clone());
@@ -230,15 +275,15 @@ fn all_light_are_off(lights: &[bool]) -> bool {
 fn all_joltage_are_zero(joltage: &[u16]) -> bool {
     joltage.iter().all(|j| *j == 0)
 }
-fn parse(mode: InputMode) -> Vec<Machine>{
+fn parse(mode: InputMode) -> Vec<Machine> {
     let input: String;
     match mode {
         InputMode::Example => {
-            input =
-                "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+            input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-[#####] (0,1,3) (0,3,4) (0,4) (2,3,4) (0,1,2) (0,1,3,4) (1,2,3,4) {17,19,27,35,35}".to_string();
+[#####] (0,1,3) (0,3,4) (0,4) (2,3,4) (0,1,2) (0,1,3,4) (1,2,3,4) {17,19,27,35,35}"
+                .to_string();
         }
         InputMode::Normal => {
             input = super::load_input("input/input-day10");
@@ -248,36 +293,54 @@ fn parse(mode: InputMode) -> Vec<Machine>{
 }
 
 pub fn part_one() {
-    let mut output: Vec<usize> = vec![];
-    let  max_deep_search = 11;
-    let machines = parse(InputMode::Normal);
-    
-    for (n, m) in machines.iter().enumerate() {
-        let single_output = m.button_light_combo_only_length(0, &m.target_lights, &mut vec![], &mut max_deep_search.clone());
-        println!("Machine #{n}: {single_output}");
-        output.push(single_output);
-    }
-    println!("Output: {}", output.iter().sum::<usize>());
-}
-
-pub fn part_two() {
-    let mut output: Vec<usize> = vec![];
-    let max_deep_search = 10;
-    let machines = parse(InputMode::Example);
-
-    let test = 0;
-    let best_button = machines[test].most_beneficial_buttons_for_joltage(&machines[test].target_joltage);
-    println!("Best button for Joltage, machine #{test}:");
-    println!("{:?} - {:?}", machines[test].buttons, machines[test].target_joltage);
-    for (b_id, score) in best_button {
-        println!("Button: {:?} - score: {}", machines[test].buttons[b_id], score);
-    }
-    let single_output = machines[test].button_joltage_combo_only_length(0, &machines[test].target_joltage, &mut 1000, max_deep_search);
+// DOESNT WOOOOOOORK
+//
+//     let mut output: Vec<usize> = vec![];
+//     let max_deep_search = 11;
+//     let machines = parse(InputMode::Normal);
+//
+//     for (n, m) in machines.iter().enumerate() {
+//         let single_output = m.button_light_combo_only_length(
+//             0,
+//             &m.target_lights,
+//             &mut vec![],
+//             &mut max_deep_search.clone(),
+//         );
+//         println!("Machine #{n}: {single_output}");
+//         output.push(single_output);
+//     }
+//     println!("Output: {}", output.iter().sum::<usize>());
+// }
+//
+// pub fn part_two() {
+//     let output: Vec<usize> = vec![];
+//     let max_deep_search = 10;
+//     let machines = parse(InputMode::Example);
+//
+//     let test = 0;
+//     let best_button =
+//         machines[test].most_beneficial_buttons_for_joltage(&machines[test].target_joltage);
+//     println!("Best button for Joltage, machine #{test}:");
+//     println!(
+//         "{:?} - {:?}",
+//         machines[test].buttons, machines[test].target_joltage
+//     );
+//     for (b_id, score) in best_button {
+//         println!(
+//             "Button: {:?} - score: {}",
+//             machines[test].buttons[b_id], score
+//         );
+//     }
+//     let single_output = machines[test].button_joltage_combo_only_length(
+//         0,
+//         &machines[test].target_joltage,
+//         &mut 1000,
+//         max_deep_search,
+//     );
     // for (n, m) in machines.iter().enumerate() {
     //     let single_output = m.button_joltage_combo_only_length(0, &m.target_joltage, &mut 1000, max_deep_search);
     //     println!("Machine #{n}: {single_output}");
     //     output.push(single_output);
     // }
     // println!("Output: {}", output.iter().sum::<usize>());
-
 }
